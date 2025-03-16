4c9f9d0210ad17e6fa13fd504ab3db408f53fce7
Andres H <andreshoilbalam@gmail.com>
2025-03-15 22:52:27 -0600

    Se creo el arreglo y se esta guardando correctamente el input

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Crear un array para almacenar los nombres
+let amigos = [];
+
+function agregarAmigo() {
+
+    const nombres = document.getElementById('amigo').value.trim(); //defino como constante los nombres, porque en teoria no cambian.
+    //.trim() quita los espacios al inicio y final del texto.
+
+    if (nombres =="" || nombres == " ") { //se evalua si esta ingresando un valor vacio o espacios.   
+        alert("Campo Vacio ingresar nombres");
+    }else{
+        amigos.push(nombres);   
+        console.log(amigos);
+    }
+   
+}
+
